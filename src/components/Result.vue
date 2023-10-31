5cc4518d48ea643079af0109a39be09ba754d712
axel-paillaud <contact@axelweb.fr>
2023-10-31 11:39:47 +0100

    première version calcl taux d'emprunt ok

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref, watch } from 'vue';
+import { ref, watch, computed } from 'vue';
+import { scoreType, scoreEnergy, scoreKilometer, scoreYear } from '@/composables/scores.js';
 const props = defineProps({
     car: Object,
     type: String,
@@ -8,16 +9,58 @@ const props = defineProps({
     year: Number,
 });
 
+const rate = ref(null);
+const score = ref(0);
+
+watch(props.car, (oldCar, newCar) => {
+    score.value = scoreType[newCar.type] + scoreEnergy[newCar.energy];
+
+    if (newCar.kilometer <= 10000) score.value += 9;
+    else if (newCar.kilometer <= 15000) score.value += 7;
+    else if (newCar.kilometer <= 20000) score.value += 5;
+    else if (newCar.kilometer <= 25000) score.value += 3;
+    else score.value += 1;
+
+    if (newCar.year <= 1970) score.value += 1;
+    else if (newCar.year <= 1990) score.value += 2;
+    else if (newCar.year <= 1980) score.value += 2;
+    else if (newCar.year <= 2000) score.value += 4;
+    else if (newCar.year <= 2000) score.value += 5;
+    else score.value += 7;
+
+    console.log(score.value);
+});
+
+const updatedRate = computed(() => {
+    if (score.value <= 10) return 3; 
+    else if (score.value <= 15) return 2.74; 
+    else if (score.value <= 25) return 2.52; 
+    else if (score.value <= 33) return 2.10; 
+    else return 1.85; 
+
+});
+
 </script>
 
 <template>
-    <p>Im the result</p>
-    <p>{{ car.type }}</p>
-    <p>{{ car.energy }}</p>
-    <p>{{ car.kilometer }}</p>
-    <p>{{ car.year }}</p>
+    <div class="rate-container">
+        <div v-if="!car.type">
+            <p>Veuillez remplir tout les champs du formulaire pour calculer votre taux d'emprunt.</p>
+        </div>
+        <div v-else>
+            <p>Votre taux d'emprunt :</p>
+            <p class="rate-value" v-if="updatedRate">{{ updatedRate }} %</p>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+.rate-container {
+    width: 200px;
+}
+.rate-value {
+    font-family: oswald;
+    font-size: 68px;
+}
 
 </style>
---
+++ src/composables/scores.js
@@ -0,0 +1,30 @@
+export const scoreType = {
+    "light-car": 8,
+    "convertible-car": 6,
+    "sedan-car": 6.5,
+    "heavy-car": 4
+}
+
+export const scoreEnergy = {
+    "petrol": 5,
+    "electric": 9,
+    "gas": 6,
+    "diesel": 4,
+    "hybrid": 7,
+}
+
+export const scoreKilometer = {
+    10000: 9,
+    15000: 7,
+    20000: 5,
+    25000: 3,
+    30000: 1,
+}
+
+export const scoreYear = {
+    1960: 1,
+    1970: 2,
+    1990: 4,
+    2000: 5,
+    2010: 7,
+}
